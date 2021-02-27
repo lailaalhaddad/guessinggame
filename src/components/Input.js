500bd95f748f0683c99b6a17194a53026cdf817f
lailaalhaddad <lalhaddad95@gmail.com>
2021-02-27 17:47:26 +0300

    stage 2 done

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,10 +2,15 @@ import React from "react";
 import { useState } from "react";
 
 const Input = () => {
-  const [numbers, setNumbers] = useState("");
+  const [numbers, setNumbers] = useState(0);
   const takenumber = () => {
-    if (numbers === "22") return alert("You win!");
+    if (numbers === 22) return alert("You win!");
     else alert("You lose :)");
+  };
+  const [attempt, attemptState] = useState(1);
+  const finaliz = () => {
+    if (attempt === 5) return alert("You loses the game.");
+    else attemptState(attempt + 1);
   };
   return (
     <div>
@@ -13,7 +18,7 @@ const Input = () => {
         placeholder="Guess your number"
         onChange={(event) => setNumbers(event.target.value)}
       />
-      <button onClick={takenumber}>Submit</button>
+      <button onClick={(takenumber, finaliz)}>Submit</button>
     </div>
   );
 };
